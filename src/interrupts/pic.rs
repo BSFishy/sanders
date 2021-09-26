ab35b92d9fbdd319010516fe9f2857b71915c650
Matt Provost <mattprovost6@gmail.com>
2021-09-25 19:28:10 -0500

    chore: remove unnecessary comment

--- src/interrupts/pic.rs
+++ src/interrupts/pic.rs
@@ -43,9 +43,6 @@ pub(crate) fn prepare_idt(idt: &mut InterruptDescriptorTable) {
 }
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFrame) {
-    // #[cfg(debug_assertions)]
-    // print!(".");
-
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
