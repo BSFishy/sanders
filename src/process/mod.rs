ed1f6df5b92ba0778c74edd5bec118e81d5bab99
Matt Provost <mattprovost6@gmail.com>
2021-09-25 19:31:42 -0500

    feat: initial implementation of processes

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -1,1 +1,20 @@
 //! TODO(BSFishy): document this
+
+/// TODO(BSFishy): document this
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub enum ProcessStatus {
+    /// TODO(BSFishy): document this
+    WaitingForStart = 0,
+    /// TODO(BSFishy): document this
+    Waiting,
+    /// TODO(BSFishy): document this
+    Running,
+}
+
+/// TODO(BSFishy): document this
+pub struct Process {
+    status: ProcessStatus,
+    ebp: u32,
+    esp: u32,
+    pointer: fn(),
+}
